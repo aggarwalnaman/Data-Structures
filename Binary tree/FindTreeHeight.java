418b8a5885f878fa39031787d806627ce8c211e8
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-08-30 00:39:15 +0530

    Find height of a tree

--- Binary tree/FindTreeHeight.java
+++ Binary tree/FindTreeHeight.java
@@ -4,38 +4,41 @@ public class FindTreeHeight{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
-        int[] a=new int[t];
-        int[] n= new int[t];
-        int[] c= new int[t];
+        List<Integer> a = new ArrayList<>(t);
+        List<Integer> n = new ArrayList<>(t);
+        List<Integer> c = new ArrayList<>(t);
         int h=1;
         for(int i=0;i<t;i++){
-            a[i]=in.nextInt();
-            n[i]=-5;
+            a.add(in.nextInt());
+            n.add(-5);
+            c.add(0);
         }
-        n[0]=0;
+        n.set(0,0);
+      
         for(int i=0;i<t;i++){
-            if(a[i]==-1){
-                n[0]=i;
+            if(a.get(i)==-1){
+                n.set(0,i);
                 break;
             }
         }
         for(int i=0;i<t;i++){
             int k=0;
             for(int j=0;j<t;j++){
-                int l=a[j];
-                if(IntStream.of(n).anyMatch(x -> x == l) ){
-                    c[k]=j;
-                    k++;
-                    // System.out.print(j+" ");
-                }
+                int l=a.get(j);
+                if(n.contains(l) ){
+                    c.set(k,j);
+                    
+                     //System.out.print(c.get(k)+" ");
+                     k++;
+                    }
             }
             for(int j=0;j<t;j++){
                 if(j<k){
-                    n[j]=c[j];
-                    System.out.print(n[j]+" ");
+                    n.set(j, c.get(j));
+                   
                 }
                 else
-                    n[j]=-5;
+                    n.set(j, -5);
             }
             System.out.println();
             if(k>0){
@@ -46,6 +49,6 @@ public class FindTreeHeight{
             }
         }
         System.out.println(h);
-        in.close();
+        
     }
 }
