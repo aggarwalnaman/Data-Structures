1956540aac090a31ff1a19a716a572d41f6b2fe5
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-09-01 17:50:03 +0530

    Arrays

---
+++ Arrays/insert_at_i.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class insert_at_i
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int size;
+        System.out.println("Enter size of array");
+        size=in.nextInt();
+        int a[]=new int[size+1];
+        System.out.println("Enter array");
+        for(int i=0;i<size;i++){
+            a[i]=in.nextInt();
+        }
+        System.out.println("Enter element to be inserted and pos:");
+        int n=in.nextInt();
+        int p=in.nextInt();
+        for(int i=size;i>p;i--){
+            a[i]=a[i-1];
+        }
+        a[p]=n;
+        System.out.println("Array:");
+        for(int i=0;i<=size;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
