446deef9d8664e2bc6aeb4b910d906e84eee9de1
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-09-04 23:27:27 +0530

    Queue Using Linked List

---
+++ Queue/queueUsingLinkedList.java
@@ -0,0 +1,72 @@
+import java.util.*;
+class Node{
+        int data;
+        Node next;
+        Node(){
+            next = null;
+        }
+        Node(int d){
+            data = d;
+            next = null;
+        }
+        Node(int d, Node n){
+            data = d;
+            next = n;
+        }
+    }
+    class queue{
+        Node rear,front;
+        {
+            rear=null;
+            front=null;
+        }
+    
+    boolean isEmpty()
+    {
+        if(rear==null && front==null) return true;
+        return false;
+    }
+    void push(int val){
+        Node nn=new Node(val);
+        if(nn==null) 
+            return;
+        if(front==null) front=rear=nn;
+        else
+        {
+            rear.next=nn;
+            rear=rear.next;
+        }
+    }
+    int pop(){
+        if (front == null) 
+                return 0; 
+      
+            
+            Node temp = front; 
+            front = front.next; 
+      
+            if (front == null) 
+                rear = null;
+            return temp.data; 
+    }
+    
+    }
+public class queueUsingLinkedList
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        queue q=new queue();
+        q.isEmpty();
+        System.out.println("Enter number of elements");
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            q.push(in.nextInt());
+        }
+        for(int i=0;i<n;i++){
+            System.out.println((q.pop()));
+        }
+        
+    }
+}
+
+
