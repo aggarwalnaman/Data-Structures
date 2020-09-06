bb38a67265b82a00ebaa33df0bbe780870e7785a
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-09-06 17:13:15 +0530

    Sorting

--- Sorting Algorithms/quickSort.java
+++ Sorting Algorithms/quickSort.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class quickSort 
 { 
     int partition(int arr[], int low, int high) 
@@ -11,31 +10,25 @@ public class quickSort
             if (arr[j] < pivot) 
             { 
                 i++; 
-  
                 int temp = arr[i]; 
                 arr[i] = arr[j]; 
                 arr[j] = temp; 
             } 
         } 
-  
         int temp = arr[i+1]; 
         arr[i+1] = arr[high]; 
         arr[high] = temp; 
-  
         return i+1; 
     } 
-  
       void sort(int arr[], int low, int high) 
     { 
         if (low < high) 
         { 
             int pi = partition(arr, low, high); 
-  
             sort(arr, low, pi-1); 
             sort(arr, pi+1, high); 
         } 
     } 
-  
     static void printArray(int arr[]) 
     { 
         int n = arr.length; 
@@ -43,15 +36,12 @@ public class quickSort
             System.out.print(arr[i]+" "); 
         System.out.println(); 
     } 
-  
     public static void main(String args[]) 
     { 
         int arr[] = {10, 7, 8, 9, 1, 5}; 
         int n = arr.length; 
-  
         quickSort ob = new quickSort(); 
         ob.sort(arr, 0, n-1); 
-  
         System.out.println("sorted array"); 
         printArray(arr); 
     } 
